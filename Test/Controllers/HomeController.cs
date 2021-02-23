5678a2062483dff0a459446ab89d7f79b28f970a
pooyay76 <pooyay76@gmail.com>
2021-02-23 06:38:30 +0330

    added contact and fixed html files

--- Test/Controllers/HomeController.cs
+++ Test/Controllers/HomeController.cs
@@ -1,21 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using Test.Models;
 
 namespace Test.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController()
         {
-            _logger = logger;
         }
 
         public IActionResult Index()
@@ -23,7 +16,7 @@ namespace Test.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Contact()
         {
             return View();
         }
---
+++ Test/Models/Class.cs
@@ -0,0 +1,7 @@
+﻿namespace Test.Models
+{
+    public class Projects
+    {
+
+    }
+}
