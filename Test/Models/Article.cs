7bc2011ebcf7eaf3232794f1ffebb15d1628015b
pooyay76 <pooyay76@gmail.com>
2021-02-23 18:03:30 +0330

    added models Project and Article

---
+++ Test/Models/Article.cs
@@ -0,0 +1,10 @@
+﻿namespace Test.Models
+{
+    public class Article
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+    }
+}
---
+++ Test/Models/Project.cs
@@ -0,0 +1,9 @@
+﻿namespace Test.Models
+{
+    public class Project
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
