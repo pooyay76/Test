b18008edcf0f83e5c2393f0e33d1a1847b0f7e1b
pooyay76 <pooyay76@gmail.com>
2021-02-23 19:22:53 +0330

    Added ProjectViewComponent

--- Test/Models/Project.cs
+++ Test/Models/Project.cs
@@ -5,5 +5,17 @@
         public long Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
+        public string Client { get; set; }
+
+        public string Image { get; set; }
+
+        public Project(long id, string title, string description, string client, string image)
+        {
+            Id = id;
+            Title = title;
+            Description = description;
+            Client = client;
+            Image = image;
+        }
     }
 }
---
+++ Test/ViewComponents/ProjectsViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Test.Models;
+
+namespace Test.ViewComponents
+{
+    public class ProjectsViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var projects = new List<Project>{
+            new Project(1,"Brilliant Trader","AI Generated FOREX Signal","Mr.x","project-1.jpg"),
+            new Project(2, "Librarian", "All Purpose Learning Center", "Mr.x","project-2.jpg"),
+            new Project(3,"Inside Job","We Help You Find Your Dream Job!","Mr.x","project-3.jpg"),
+            new Project(4,"Companion","Smart House Developer","Mr.x","project-4.jpg")
+            };
+            return View("_Projects",projects);
+        }
+    }
+}
